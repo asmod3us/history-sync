1df96ec3596366fb845a5abc336700f0f4f8238f
asmod3us <4sm0d3us@gmail.com>
2021-01-18 22:40:56 +0100

    use env vars if defined, fall back to default values

--- history-sync.plugin.zsh
+++ history-sync.plugin.zsh
@@ -21,12 +21,12 @@ alias zhsync="history_sync_pull && history_sync_push"
 GIT=$(which git)
 GPG=$(which gpg)
 
-ZSH_HISTORY_PROJ="${HOME}/.zsh_history_proj"
-ZSH_HISTORY_FILE_NAME=".zsh_history"
-ZSH_HISTORY_FILE="${HOME}/${ZSH_HISTORY_FILE_NAME}"
-ZSH_HISTORY_FILE_ENC_NAME="zsh_history"
-ZSH_HISTORY_FILE_ENC="${ZSH_HISTORY_PROJ}/${ZSH_HISTORY_FILE_ENC_NAME}"
-ZSH_HISTORY_FILE_DECRYPT_NAME="zsh_history_decrypted"
+ZSH_HISTORY_PROJ=${ZSH_HISTORY_PROJ:-"${HOME}/.zsh_history_proj"}
+ZSH_HISTORY_FILE_NAME=${ZSH_HISTORY_FILE_NAME:-".zsh_history"}
+ZSH_HISTORY_FILE=${ZSH_HISTORY_FILE:-"${HOME}/${ZSH_HISTORY_FILE_NAME}"}
+ZSH_HISTORY_FILE_ENC_NAME=${ZSH_HISTORY_FILE_ENC_NAME:-"zsh_history"}
+ZSH_HISTORY_FILE_ENC=${ZSH_HISTORY_FILE_ENC:-"${ZSH_HISTORY_PROJ}/${ZSH_HISTORY_FILE_ENC_NAME}"}
+ZSH_HISTORY_FILE_DECRYPT_NAME=${ZSH_HISTORY_FILE_DECRYPT_NAME:-"zsh_history_decrypted"}
 GIT_COMMIT_MSG="latest $(date)"
 
 function _print_git_error_msg() {
